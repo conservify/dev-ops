d0eb88470842b31a7da4951f6926c0dfb69ec4ea
Jacob Lewallen <jlewallen@gmail.com>
2018-08-08 20:24:28 -0700

    Copy distribution binaries from build.

--- vars/refreshDistribution.groovy
+++ vars/refreshDistribution.groovy
@@ -2,8 +2,9 @@
 
 def call(Map parameters = [:]) {
     node ('master') {
-        dir ("../distribution") {
-            sh "build/artifacts-publisher --source ~/jobs --destination /var/lib/distribution"
+        lock("distribution") {
+            copyArtifacts(projectName: 'distribution', flatten: true)
+            sh "./artifacts-publisher --source ~/jobs --destination /var/lib/distribution"
         }
     }
 
