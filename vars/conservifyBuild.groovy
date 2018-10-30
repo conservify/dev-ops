a7bbfafe8885089f9c381f0656aa17b8d5dfb576
Jacob Lewallen <jlewallen@gmail.com>
2018-10-30 15:18:29 -0700

    Add slack notifications to conservifyBuild.

--- vars/conservifyBuild.groovy
+++ vars/conservifyBuild.groovy
@@ -10,45 +10,51 @@ def call(Map parameters = [:]) {
         error 'conservifyBuild: Name is required'
     }
 
-    stage ('git') {
-        if (repository) {
-            git branch: 'master', url: repository
+    try {
+        stage ('git') {
+            if (repository) {
+                git branch: 'master', url: repository
+            }
+            else {
+                checkout scm
+            }
         }
-        else {
-            checkout scm
-        }
-    }
 
-    stage ('clean') {
-        sh "rm -rf gitdeps"
-        sh "make clean"
-    }
-
-    stage ('deps') {
-        def files = findFiles(glob: '**/arduino-libraries, **/dependencies.sd')
-        if (files.length > 0) {
+        stage ('clean') {
             sh "rm -rf gitdeps"
-            sh "make gitdeps"
+            sh "make clean"
         }
-    }
 
-    stage ('build') {
-        sh "make"
-    }
-
-    if (archive instanceof String) {
-        stage ('archive') {
-            archiveArtifacts artifacts: archive
+        stage ('deps') {
+            def files = findFiles(glob: '**/arduino-libraries, **/dependencies.sd')
+            if (files.length > 0) {
+                sh "rm -rf gitdeps"
+                sh "make gitdeps"
+            }
         }
-    }
 
-    if (parameters.test) {
-        stage ('test') {
-            sh "make test"
+        stage ('build') {
+            sh "make"
         }
-    }
 
-    // slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal (<${env.BUILD_URL}|Open>)"
+        if (archive instanceof String) {
+            stage ('archive') {
+                archiveArtifacts artifacts: archive
+            }
+        }
+
+        if (parameters.test) {
+            stage ('test') {
+                sh "make test"
+            }
+        }
+
+        slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Success (<${env.BUILD_URL}|Open>)"
+    }
+    catch (Exception e) {
+        slackSend channel: '#automation', color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failed (<${env.BUILD_URL}|Open>)"
+        throw e;
+    }
 
     return
 }
