71559bc74334f57d73cb7949866b37b66fd55411
Jacob Lewallen <jlewallen@gmail.com>
2019-05-15 10:47:31 -0700

    Allow disabling of triggers.

--- vars/conservifyProperties.groovy
+++ vars/conservifyProperties.groovy
@@ -1,10 +1,15 @@
 #!/usr/bin/env groovy
 
-def call(List additional = []) {
+def call(List additional = [], Map parameters = [:]) {
     def props = [] + additional
 
     props.add([$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', numToKeepStr: '5']])
-    props.add(pipelineTriggers([cron('@weekly'), githubPush()]))
+
+    if (parameters.manual) {
+    }
+    else {
+        props.add(pipelineTriggers([cron('@weekly'), githubPush()]))
+    }
 
     properties(props)
 }
