db9edad46b4bbfc6095fad8d27cf1da854b05af2
Jacob Lewallen <jlewallen@gmail.com>
2019-05-15 10:50:41 -0700

    Add some logging.

--- vars/conservifyProperties.groovy
+++ vars/conservifyProperties.groovy
@@ -3,11 +3,15 @@
 def call(List additional = [], Map parameters = [:]) {
     def props = [] + additional
 
+    echo 'Applying Conservify properties'
+
     props.add([$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', numToKeepStr: '5']])
 
     if (parameters.manual) {
+        echo 'Manual triggers enabled.'
     }
     else {
+        echo 'Configuring default triggers.'
         props.add(pipelineTriggers([cron('@weekly'), githubPush()]))
     }
 
