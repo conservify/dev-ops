b19b082f8ca7091123da2253c57fd6523f5b05f0
Jacob Lewallen <jlewallen@gmail.com>
2020-01-08 16:44:28 -0800

    Keep more meta for each batch.

--- docker/diagnostics/main.go
+++ docker/diagnostics/main.go
@@ -4,10 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
+
+	"encoding/json"
 
 	"net/http"
 )
@@ -15,6 +19,90 @@ import (
 type options struct {
 	RootPath string
 	Strip    int
+}
+
+type UploadMeta struct {
+	Batch string
+	Time  time.Time
+}
+
+func saveOrReadMeta(batch, path string) (meta *UploadMeta, err error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			return nil, fmt.Errorf("error: %v", err)
+		}
+
+		defer file.Close()
+
+		meta := &UploadMeta{
+			Batch: batch,
+			Time:  time.Now(),
+		}
+
+		bytes, err := json.Marshal(meta)
+		if err != nil {
+			return nil, fmt.Errorf("error: %v", err)
+		}
+
+		_, err = file.Write(bytes)
+		if err != nil {
+			return nil, fmt.Errorf("error: %v", err)
+		}
+	} else {
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, fmt.Errorf("error: %v", err)
+		}
+
+		err = json.Unmarshal(data, meta)
+		if err != nil {
+			return nil, fmt.Errorf("error: %v", err)
+		}
+	}
+
+	return
+}
+
+func serve(o *options, w http.ResponseWriter, req *http.Request) (meta *UploadMeta, err error) {
+	log.Printf("[http] %s", req.URL)
+
+	p := strings.Split(req.URL.Path[1:], "/")
+	if len(p) != o.Strip+2 {
+		return nil, fmt.Errorf("bad path")
+	}
+
+	batch := p[o.Strip]
+	filename := p[o.Strip+1]
+	localDirectory := filepath.Join(o.RootPath, batch)
+
+	err = os.MkdirAll(localDirectory, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create directory: %v", err)
+	}
+
+	metaPath := filepath.Join(localDirectory, "meta.json")
+	meta, err = saveOrReadMeta(batch, metaPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to save or read meta: %v", err)
+	}
+
+	localPath := filepath.Join(localDirectory, filename)
+	file, err := os.Create(localPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create file: %v", err)
+	}
+
+	defer file.Close()
+
+	bytes, err := io.Copy(file, req.Body)
+	if err != nil {
+		return nil, fmt.Errorf("unable to copy file: %v", err)
+	}
+
+	log.Printf("[http] (%s) received %d -> %s", req.URL, bytes, localPath)
+
+	return meta, nil
 }
 
 func main() {
@@ -31,41 +119,22 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		log.Printf("[http] %s", req.URL)
-
-		p := strings.Split(req.URL.Path[1:], "/")
-		if len(p) != o.Strip+2 {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		batch := p[o.Strip]
-		filename := p[o.Strip+1]
-
-		localDirectory := filepath.Join(o.RootPath, batch)
-		err := os.MkdirAll(localDirectory, 0755)
+		meta, err := serve(o, w, req)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(fmt.Sprintf("error: %v", err)))
 			return
 		}
 
-		localPath := filepath.Join(localDirectory, filename)
-		file, err := os.Create(localPath)
+		bytes, err := json.Marshal(meta)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(fmt.Sprintf("error: %v", err)))
 			return
 		}
-
-		defer file.Close()
-
-		bytes, err := io.Copy(file, req.Body)
-
-		log.Printf("[http] (%s) received %d -> %s", req.URL, bytes, localPath)
 
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(fmt.Sprintf("%s", batch)))
+		w.Write(bytes)
 	})
 
 	log.Printf("starting...")
