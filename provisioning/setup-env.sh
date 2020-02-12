c91bf1dfa37fe1b8a8d756836a5d53f64e224114
Jacob Lewallen <jlewallen@gmail.com>
2020-02-12 08:40:08 -0800

    Fix output variable name.

--- provisioning/setup-env.sh
+++ provisioning/setup-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd ../terraform/fk
-export ENV_DB_URL=`terraform output db_url`
+export ENV_DB_URL=`terraform output database_url`
 export APP_SERVER_ADDRESS=`terraform output app_server_address`
 export FK_CLOUD_ROOT=~/fieldkit/cloud
 popd
