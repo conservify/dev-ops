e0f5043dc9323cb764ecab78f28fc71c4d9fce21
Jacob Lewallen <jlewallen@gmail.com>
2020-02-14 15:44:56 -0800

    Move to S3 backend.

--- terraform/fk/01-aws.tf
+++ terraform/fk/01-aws.tf
@@ -3,3 +3,12 @@ provider "aws" {
   secret_key = var.secret_key
   region     = var.region
 }
+
+terraform {
+  backend "s3" {
+	bucket = "conservify-terraform-state"
+	region = "us-west-2"
+	encrypt = true
+	key = "fk"
+  }
+}
