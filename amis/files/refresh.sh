bf98e799b5a8b95d5991a1b4c6aac0038ecc6e20
Jacob Lewallen <jlewallen@gmail.com>
2020-02-15 12:49:28 -0800

    Tag incoming images with 'run' to simplify upgrades and the compose.

--- amis/files/refresh.sh
+++ amis/files/refresh.sh
@@ -23,7 +23,23 @@ for archive in `find ${WATCHING} -name "*.tar*"`; do
 
 	# No matter which kind of image this is, try and load the docker containers inside.
 	for image in *.di; do
-		docker load < ${image} && rm ${image}
+		image_name=`basename ${image} .di`
+		echo loading ${image}
+
+		# So we load the image here and then parse the image name and
+		# tag from the output of the load call, here's an example of
+		# the output we'll get:
+		# Loaded image: conservify/fk-cloud-proxy:20200213-225502
+		loaded=`docker load -i ${image} | sed -n 's/^Loaded image: \([0-9a-f]*\)/\1/p'`
+		service=`echo ${loaded} | awk -F':' '{print $1}'`
+		tag=`echo ${loaded} | awk -F':' '{print $2}'`
+
+		# Remove the old 'active' tag and then tag this new one.
+		echo loaded ${loaded}...
+		docker rmi ${service}:active || true
+		docker tag ${loaded} ${service}:active
+
+		rm ${image}
 	done
 
 	# Is this a stack of services in a docker-compose bundle?
