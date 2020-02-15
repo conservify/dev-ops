57a6d5beb609e8a8235de2663474f1ccb52cb500
Jacob Lewallen <jlewallen@gmail.com>
2020-02-15 10:50:43 -0800

    Run ingester alongside.

--- amis/services/fk-cloud-service/startup.sh
+++ amis/services/fk-cloud-service/startup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-/app/server &
+echo starting cloud service..
+echo args: $@
+
+EXEC=$1
+
+if [ -f $EXEC ]; then
+	$EXEC &
+fi
 
 envoy -c /etc/service-envoy.yaml --service-cluster fk-cloud
