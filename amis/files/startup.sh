b2b4c66c2178e10270608f16a2261fd3b6feb8a0
Jacob Lewallen <jlewallen@gmail.com>
2020-02-16 14:32:33 -0800

    Startup script that downloads the stack in user-data.

---
+++ amis/files/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /etc/user_data.env
+
+if [ ! -z "$APPLICATION_STACK" ]; then
+	mkdir -p /tmp/incoming-stacks
+	mkdir -p /tmp/downloading-stacks
+	pushd /tmp/downloading-stacks
+	wget --auth-no-challenge $APPLICATION_STACK
+	mv * /tmp/incoming-stacks
+	popd
+fi
