e726a8b95350825a16a16fe3546f1b337dec84bf
Jacob Lewallen <jlewallen@gmail.com>
2020-02-17 12:44:21 -0800

    Skip old tables.

--- provisioning/db-dump.sh
+++ provisioning/db-dump.sh
@@ -9,5 +9,5 @@ if [ -z "$ENV_DB_URL" ]; then
 fi
 
 ssh -t fkdev "docker run -it --rm postgres pg_dump -s $ENV_DB_URL > schema.sql"
-ssh -t fkdev "docker run -it --rm postgres pg_dump -a $ENV_DB_URL --exclude-table-data fieldkit.inaturalist_observations > data.sql"
+ssh -t fkdev "docker run -it --rm postgres pg_dump -a $ENV_DB_URL --exclude-table-data fieldkit.record_analysis --exclude-table-data fieldkit.record --exclude-table-data fieldkit.inaturalist_observations > data.sql"
 rsync -zvua --progress "fkdev:*.sql" ../
