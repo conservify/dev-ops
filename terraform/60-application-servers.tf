ce62ce72d911339db3f8d96a20161d3f917e98da
Jacob Lewallen <jlewallen@gmail.com>
2020-02-19 07:50:52 -0800

    Style/formatting.

--- terraform/60-application-servers.tf
+++ terraform/60-application-servers.tf
@@ -9,26 +9,26 @@ data "template_file" "app_server_user_data" {
   template               = file("user_data.yaml")
 
   vars = {
-	hostname             = "${each.value.name}"
-	zone_name            = "${local.zone.name}"
-	env_tag              = "${local.env}"
+	hostname             = each.value.name
+	zone_name            = local.zone.name
+	env_tag              = local.env
 
-	aws_access_key       = "${var.access_key}"
-	aws_secret_key       = "${var.secret_key}"
+	aws_access_key       = var.access_key
+	aws_secret_key       = var.secret_key
 
-	gelf_url             = "${var.gelf_url}"
-	statsd_address       = "${var.statsd_address}"
+	gelf_url             = var.gelf_url
+	statsd_address       = var.statsd_address
 
 
-	influx_url           = "${var.influx_database.url}"
-	influx_database      = "${var.influx_database.name}"
-	influx_user          = "${var.influx_database.user}"
-	influx_password      = "${var.influx_database.password}"
+	influx_url           = var.influx_database.url
+	influx_database      = var.influx_database.name
+	influx_user          = var.influx_database.user
+	influx_password      = var.influx_database.password
 
-	application_start    = "${each.value.config.start}"
-	application_stack    = "${each.value.config.stack}"
+	application_start    = each.value.config.start
+	application_stack    = each.value.config.stack
 
-	database_url         = "${local.database_url}"
+	database_url         = local.database_url
   }
 }
 
@@ -38,7 +38,7 @@ resource "aws_instance" "app-servers" {
   ami                         = data.aws_ami.bare.id
   subnet_id                   = aws_subnet.private[each.value.zone].id
   instance_type               = each.value.config.instance
-  vpc_security_group_ids      = ["${aws_security_group.ssh.id}", "${aws_security_group.fk-app-server.id}"]
+  vpc_security_group_ids      = [ aws_security_group.ssh.id, aws_security_group.fk-app-server.id ]
   user_data                   = data.template_file.app_server_user_data[each.key].rendered
   monitoring                  = true
   associate_public_ip_address = false
