afe0cc66ea7c3483df746dc788fd686c979feb90
Jacob Lewallen <jlewallen@gmail.com>
2020-02-27 18:29:05 -0800

    Copy static.env around when building stacks.

--- amis/services/fk-cloud-service/startup.sh
+++ amis/services/fk-cloud-service/startup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-echo starting cloud service..
-echo args: $@
+echo starting cloud service: $@
 
 EXEC=$1
+
+if [ -f /etc/static.env ]; then
+	echo found /etc/static.env
+	source /etc/static.env
+fi
 
 if [ -f $EXEC ]; then
 	$EXEC &
