e89c0fe6c9e3576769f1dcaa738fe4cd1f84f9a5
Jacob Lewallen <jlewallen@gmail.com>
2020-03-09 16:13:34 -0700

    Remove stage around distributeFirmware

--- vars/distributeFirmware.groovy
+++ vars/distributeFirmware.groovy
@@ -1,37 +1,35 @@
 #!/usr/bin/env groovy
 
 def call(Map parameters = [:]) {
-    stage ('distribute') {
-        def command = "fktool --scheme https"
+	def command = "fktool --scheme https"
 
-		if (parameters.directory) {
-		   command += " --firmware-directory " + parameters.directory
-		}
+	if (parameters.directory) {
+		command += " --firmware-directory " + parameters.directory
+	}
 
-		if (parameters.file) {
-		   command += " --firmware-file " + parameters.file
-		}
+	if (parameters.file) {
+		command += " --firmware-file " + parameters.file
+	}
 
-        if (parameters.module) {
-            command += " --module " + parameters.module
-        }
+	if (parameters.module) {
+		command += " --module " + parameters.module
+	}
 
-        if (parameters.profile) {
-            command += " --profile " + parameters.profile
-        }
+	if (parameters.profile) {
+		command += " --profile " + parameters.profile
+	}
 
-		if (parameters.email) {
-			command += " --email " + parameters.email
-		}
+	if (parameters.email) {
+		command += " --email " + parameters.email
+	}
 
-		if (parameters.password) {
-			command += " --password " + parameters.password
-		}
+	if (parameters.password) {
+		command += " --password " + parameters.password
+	}
 
-		echo command + " --host api.fkdev.org"
-		sh command + " --host api.fkdev.org"
+	echo command + " --host api.fkdev.org"
+	sh command + " --host api.fkdev.org"
 
-		echo command + " --host api.fieldkit.org"
-		sh command + " --host api.fieldkit.org"
-    }
+	echo command + " --host api.fieldkit.org"
+	sh command + " --host api.fieldkit.org"
 }
