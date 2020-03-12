2020eb5551280724db3616e99dbe46ec4c155a13
Jacob Lewallen <jlewallen@gmail.com>
2020-03-12 12:46:58 -0700

    Style on home.

--- docker/diagnostics/static/Home.vue
+++ docker/diagnostics/static/Home.vue
@@ -1,26 +1,28 @@
 <!-- Home.vue -->
 <template>
     <div class="container">
-        <table class="archives">
-            <thead>
-                <tr>
-                    <th class="track" style="">Time</th>
-                    <th class="artist" style="">Phrase</th>
-                    <th class="download" style=""></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="archive in archives" class="archive">
-                    <td class="time">{{ archive.time | prettyTime }}</td>
-                    <td class="phrase">
-                        <a :href="'?id=' + archive.id" v-on:click.prevent="view(archive)">{{ archive.phrase }}</a>
-                    </td>
-                    <td class="download">
-                        <a :href="'/diagnostics/archives/' + archive.id + '.zip?token=' + token">Download</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="col-md-12">
+            <table class="archives">
+                <thead>
+                    <tr>
+                        <th class="track" style="">Time</th>
+                        <th class="artist" style="">Phrase</th>
+                        <th class="download" style=""></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="archive in archives" class="archive">
+                        <td class="time">{{ archive.time | prettyTime }}</td>
+                        <td class="phrase">
+                            <a :href="'?id=' + archive.id" v-on:click.prevent="view(archive)">{{ archive.phrase }}</a>
+                        </td>
+                        <td class="download">
+                            <a :href="'/diagnostics/archives/' + archive.id + '.zip?token=' + token">Download</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <script>
@@ -29,9 +31,9 @@ import moment from 'moment'
 export default {
     name: 'Home',
     props: {
-		query: {
+        query: {
             required: true,
-		},
+        },
         token: {
             required: true,
         },
@@ -42,28 +44,27 @@ export default {
         }
     },
     created() {
-		const options = {
-			headers: {
-				Authorization: this.token,
-			},
-		}
+        const options = {
+            headers: {
+                Authorization: this.token,
+            },
+        }
 
-		fetch('archives?q=' + (this.query.q || ""), options)
+        fetch('archives?q=' + (this.query.q || ''), options)
             .then(response => response.json())
-			.then(archives => {
-				if (archives.archives.length == 1) {
-					this.$emit('navigate', '?id=' + archives.archives[0].id)
-				}
-				else {
-					this.archives = archives.archives
-				}
-			})
+            .then(archives => {
+                if (archives.archives.length == 1) {
+                    this.$emit('navigate', '?id=' + archives.archives[0].id)
+                } else {
+                    this.archives = archives.archives
+                }
+            })
     },
     filters: {
         prettyTime(value) {
             return moment(value).format('MMM Do YYYY hh:mm:ss')
         },
-	},
+    },
     methods: {
         view(archive) {
             this.$emit('navigate', '?id=' + archive.id)
@@ -72,7 +73,7 @@ export default {
 }
 </script>
 <style>
-	table.archives {
-	width: 100%;
-	}
+table.archives {
+    width: 100%;
+}
 </style>
