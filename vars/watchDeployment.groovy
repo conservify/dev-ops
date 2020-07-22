d7fbf231636d5345a8b2840759a600b4211e64cf
Jacob Lewallen <jlewallen@gmail.com>
2020-07-22 10:36:47 -0700

    Starting on a watchDeployment helper.

---
+++ vars/watchDeployment.groovy
@@ -0,0 +1,47 @@
+#!/usr/bin/env groovy
+
+import groovy.json.JsonSlurper
+
+@NonCPS
+def getStatusHash(String url) {
+	def get = new URL(url).openConnection();
+	def httpStatus = get.getResponseCode();
+	if (!httpStatus.equals(200)) {
+		return null
+	}
+	def json = new JsonSlurper().parseText(get.getInputStream().getText())
+	return json.git.hash
+}
+
+def call(Map parameters = [:]) {
+    try {
+        stage ('watch-deploy') {
+			def counter = 0
+			def previous = getStatusHash(parameters.url)
+			if (!previous) {
+				slackSend channel: '#automation', color: 'danger', message: "No status: " + parameters.url
+				return false
+			}
+			while (counter < 10) {
+				def gitHash = getStatusHash(parameters.url)
+				if (gitHash) {
+					println(gitHash);
+					if (previous != gitHash) {
+						slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Deployed And Ready (<${env.BUILD_URL}|Open>)"
+						return true
+					}
+				}
+				sleep(1)
+				counter += 1
+			}
+
+			// slackSend channel: '#automation', color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Oh, no. Deploy took too long (<${env.BUILD_URL}|Open>)"
+			return false
+		}
+	}
+	catch (Exception e) {
+		throw e;
+	}
+
+	return
+}
