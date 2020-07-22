ad14eab5d51ae463b18875f3670de50be5ba58ae
Jacob Lewallen <jlewallen@gmail.com>
2020-07-22 11:11:18 -0700

    Configurable seconds to wait for.

--- vars/watchDeployment.groovy
+++ vars/watchDeployment.groovy
@@ -16,18 +16,18 @@ def getStatusHash(String url) {
 def call(Map parameters = [:]) {
     try {
         stage ('watch-deploy') {
+			def seconds = parameters.seconds ?: 30
 			def counter = 0
 			def previous = getStatusHash(parameters.url)
 			if (!previous) {
 				slackSend channel: '#automation', color: 'danger', message: "No status: " + parameters.url
 				return false
 			}
-			while (counter < 10) {
+			while (counter < seconds) {
 				def gitHash = getStatusHash(parameters.url)
 				if (gitHash) {
 					println(gitHash);
 					if (previous != gitHash) {
-						slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Deployed And Ready (<${env.BUILD_URL}|Open>)"
 						return true
 					}
 				}
@@ -35,7 +35,6 @@ def call(Map parameters = [:]) {
 				counter += 1
 			}
 
-			// slackSend channel: '#automation', color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Oh, no. Deploy took too long (<${env.BUILD_URL}|Open>)"
 			return false
 		}
 	}
