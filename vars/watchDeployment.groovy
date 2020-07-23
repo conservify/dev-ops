c1c4c882e85d6059734ba3a4ba10ebd9bf459085
Jacob Lewallen <jlewallen@gmail.com>
2020-07-23 10:20:34 -0700

    Duh, simplifying this watch script.

--- vars/watchDeployment.groovy
+++ vars/watchDeployment.groovy
@@ -14,40 +14,30 @@ def getStatusHash(String url) {
 }
 
 def call(Map parameters = [:]) {
-    try {
-        stage ('watch-deploy') {
-			def seconds = parameters.seconds ?: 30
-			def counter = 0
-			def previous = getStatusHash(parameters.url)
-			if (!previous) {
-				slackSend channel: '#automation', color: 'danger', message: "No status: " + parameters.url
-				return false
-			}
-			while (counter < seconds) {
-				def gitHash = getStatusHash(parameters.url)
-				if (gitHash) {
-					println(gitHash);
-					if (previous != gitHash) {
-						slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Deployed And Ready (<${env.BUILD_URL}|Open>)"
-
-						println("returning, true")
-						return true
-					}
-				}
-				sleep(1)
-				counter += 1
-			}
-
-			slackSend channel: '#automation', color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Oh, no. Deploy took too long (<${env.BUILD_URL}|Open>)"
-
-			println("returning(a), false")
+	return stage('watch-deploy') {
+		def seconds = parameters.seconds ?: 30
+		def counter = 0
+		def previous = getStatusHash(parameters.url)
+		if (!previous) {
+			slackSend channel: '#automation', color: 'danger', message: "No status: " + parameters.url
 			return false
 		}
-	}
-	catch (Exception e) {
-		throw e;
-	}
+		while (counter < seconds) {
+			def gitHash = getStatusHash(parameters.url)
+			if (gitHash) {
+				println(gitHash);
+				if (previous != gitHash) {
+					slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Deployed And Ready (<${env.BUILD_URL}|Open>)"
 
-	println("returning(b), false")
-	return false
+					return true
+				}
+			}
+			sleep(1)
+			counter += 1
+		}
+
+		slackSend channel: '#automation', color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Oh, no. Deploy took too long (<${env.BUILD_URL}|Open>)"
+
+		return false
+	}
 }
