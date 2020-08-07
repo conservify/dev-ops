423e1382c6febb01df9987dfefdb020fd342e88d
Jacob Lewallen <jlewallen@gmail.com>
2020-08-07 14:30:53 -0700

    Better script for harvesting S3 objects and a quick delete.

--- tools/harvest-keys.py
+++ tools/harvest-keys.py
@@ -3,23 +3,20 @@
 import sys
 import argparse
 import logging
-import csv
 import re
 import urllib.parse
 import json
+import boto3
 
 
 def get_keys_from_file(file, keys):
-    reader = csv.reader(file)
-    for row in reader:
-        for column in row:
-            m = re.search(r"/([\w-]+)\.s3", column)
-            if m:
-                bucket = m[1]
-                parsed = urllib.parse.urlparse(column)
-                if parsed:
-                    key = parsed.path[1:]
-                    keys.setdefault(bucket, []).append(key)
+    for line in file:
+        for m in re.findall(r"(https://([^\.]+).s3.amazonaws.com/([^\s]+))", line):
+            bucket = m[1]
+            parsed = urllib.parse.urlparse(m[0])
+            if parsed:
+                key = parsed.path[1:]
+                keys.setdefault(bucket, []).append(m[2])
     return keys
 
 
@@ -28,6 +25,7 @@ if __name__ == "__main__":
     log = logging.getLogger("harvest-keys")
 
     parser = argparse.ArgumentParser(description="s3 key tool")
+    parser.add_argument("--delete", action="store_true", default=False)
     parser.add_argument("file", type=argparse.FileType("r"), nargs="+")
     args = parser.parse_args()
 
@@ -43,3 +41,13 @@ if __name__ == "__main__":
         with open(bucket + ".keys", "w") as file:
             for key in keys[bucket]:
                 file.write("{}\n".format(key))
+
+    if args.delete:
+        cl = boto3.client("s3")
+        for bucket in keys:
+            logging.info("deleting from {}".format(bucket))
+            bucket_keys = keys[bucket]
+            object_entries = [{"Key": key} for key in keys[bucket]]
+            response = cl.delete_objects(
+                Bucket=bucket, Delete={"Objects": object_entries, "Quiet": False,},
+            )
