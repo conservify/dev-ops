f030ab719de86f100321f8835b2d8fbafe25bb4a
Jacob Lewallen <jlewallen@gmail.com>
2020-10-19 13:18:25 -0700

    Link to directly download logs.

--- docker/diagnostics/static/Archive.vue
+++ docker/diagnostics/static/Archive.vue
@@ -8,9 +8,7 @@
 
         <h4>{{ archive.time | prettyTime }}</h4>
 
-        <div class="alert alert-primary" role="alert">
-            Mobile App DB
-        </div>
+        <div class="alert alert-primary" role="alert">Mobile App DB</div>
         <div class="row" v-if="analysis">
             <div class="col-md-12">
                 <table class="table table-condensed stations">
@@ -35,13 +33,18 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-                <a target="_blank" :href="'/diagnostics/archives/' + archive.id + '/fk.db?token=' + token">Download</a>
+                <a target="_blank" :href="'/diagnostics/archives/' + archive.id + '/fk.db?token=' + token" download="fk.db">DB</a>
+                <a
+                    target="_blank"
+                    :href="'/diagnostics/archives/' + archive.id + '/app.txt?token=' + token"
+                    :download="archive.id + '.txt'"
+                >
+                    Logs
+                </a>
             </div>
         </div>
 
-        <div class="alert alert-primary" role="alert">
-            Device / Configuration
-        </div>
+        <div class="alert alert-primary" role="alert">Device / Configuration</div>
         <div class="row">
             <div class="col-md-6 device-json">
                 <vue-json-pretty :data="device" :showDoubleQuotes="false"></vue-json-pretty>
@@ -49,9 +52,7 @@
             <div class="col-md-6"></div>
         </div>
 
-        <div class="alert alert-primary" role="alert" v-if="mobileAppLogs">
-            Mobile App Logs
-        </div>
+        <div class="alert alert-primary" role="alert" v-if="mobileAppLogs">Mobile App Logs</div>
         <div class="row" v-if="mobileAppLogs">
             <div class="col-md-12">
                 <pre class="app-logs">{{ mobileAppLogs }}</pre>
@@ -96,26 +97,26 @@ export default {
         }
 
         fetch(Config.BaseUrl + 'archives/' + this.query.id, options)
-            .then(response => response.json())
-            .then(archive => {
+            .then((response) => response.json())
+            .then((archive) => {
                 this.archive = archive
             })
 
         fetch(Config.BaseUrl + 'archives/' + this.query.id + '/app.txt', options)
-            .then(response => response.text())
-            .then(mobileAppLogs => {
+            .then((response) => response.text())
+            .then((mobileAppLogs) => {
                 this.mobileAppLogs = mobileAppLogs
             })
 
         fetch(Config.BaseUrl + 'archives/' + this.query.id + '/device.json', options)
-            .then(response => response.json())
-            .then(device => {
+            .then((response) => response.json())
+            .then((device) => {
                 this.device = device
             })
 
         fetch(Config.BaseUrl + 'archives/' + this.query.id + '/analysis', options)
-            .then(response => response.json())
-            .then(analysis => {
+            .then((response) => response.json())
+            .then((analysis) => {
                 this.analysis = analysis
             })
     },
@@ -146,7 +147,7 @@ html {
     overflow-y: scroll;
 }
 .archive-container {
-	padding: 1em;
+    padding: 1em;
 }
 .device-table {
     font-size: 80%;
