07b03f12bcb059936929ee14fd2ec7f18653dc8b
Jacob Lewallen <jlewallen@gmail.com>
2021-04-28 19:02:46 -0700

    feat: Display version/description in notify success.

--- vars/notifySuccess.groovy
+++ vars/notifySuccess.groovy
@@ -1,5 +1,11 @@
 #!/usr/bin/env groovy
 
 def call() {
-    slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Success (<${env.BUILD_URL}|Open>)"
+	def maybeVersion = currentBuild.description
+	if (maybeVersion && maybeVersion != "") {
+		slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} <${env.BUILD_URL}|Success> (${maybeVersion})"
+	}
+	else {
+		slackSend channel: '#automation', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} <${env.BUILD_URL}|Success>"
+	}
 }
