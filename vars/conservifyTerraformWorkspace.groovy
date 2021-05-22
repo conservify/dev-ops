b7516218d1199d5cc3d3069b752be5bbc3936797
Jacob Lewallen <jlewallen@gmail.com>
2021-05-22 15:01:37 -0700

    feat: Specify branch.

--- vars/conservifyTerraformWorkspace.groovy
+++ vars/conservifyTerraformWorkspace.groovy
@@ -5,7 +5,7 @@ def call(Map parameters) {
 				withCredentials([file(credentialsId: "terraform.tfvars.json", variable: 'TERRAFORM_VARS_JSON')]) {
 					withAWS(credentials: 'aws-default', region: 'us-east-1') {
 						dir ('dev-ops') {
-							git url: 'https://github.com/conservify/dev-ops.git'
+							git branch: "main", url: 'https://github.com/conservify/dev-ops.git'
 							sh "mv -f $TERRAFORM_VARS_JSON terraform"
 							sh "cd terraform && TF_IN_AUTOMATION=true TF_CLI_ARGS='-no-color' make env-all"
 							sh "mv terraform/build/deploy*.json ../"
