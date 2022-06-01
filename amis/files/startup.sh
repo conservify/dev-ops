599993ff9c26fd515f27f99012416f3980ff6205
Jacob Lewallen <jlewallen@gmail.com>
2022-06-01 15:21:05 -0700

    feat: Format and mount attached volume.
    This will need to avoid reformatting.

--- amis/files/startup.sh
+++ amis/files/startup.sh
@@ -7,6 +7,12 @@ source /etc/user_data.env
 mkdir -p /tmp/incoming-stacks
 mkdir -p /tmp/downloading-stacks
 
+mkdir -p /svr0 /svr1 /svr2 /svr3 /svr4
+if [ -e /dev/xvdh ]; then
+    mkfs.ext4 /dev/xvdh
+    mount /dev/xvdh /svr0
+fi
+
 pushd /tmp/downloading-stacks
 /var/lib/conservify/startup.py --urls $APPLICATION_STACKS
 mv * /tmp/incoming-stacks
--- terraform/fk/55-influxdb.tf
+++ terraform/fk/55-influxdb.tf
@@ -3,23 +3,23 @@ data "template_file" "influxdb_server_user_data" {
   template                    = file("user_data_influxdb.yaml")
 
   vars = {
-	hostname                  = each.value.name
-	zone_name                 = local.zone.name
-	env_tag                   = local.env
+    hostname                  = each.value.name
+    zone_name                 = local.zone.name
+    env_tag                   = local.env
 
-	aws_access_key            = var.access_key
-	aws_secret_key            = var.secret_key
+    aws_access_key            = var.access_key
+    aws_secret_key            = var.secret_key
 
-	gelf_url                  = var.gelf_url
-	statsd_address            = var.statsd_address
-	metrics_influxdb_url      = var.metrics_influxdb.url
-	metrics_influxdb_database = var.metrics_influxdb.name
-	metrics_influxdb_user     = var.metrics_influxdb.user
-	metrics_influxdb_password = var.metrics_influxdb.password
+    gelf_url                  = var.gelf_url
+    statsd_address            = var.statsd_address
+    metrics_influxdb_url      = var.metrics_influxdb.url
+    metrics_influxdb_database = var.metrics_influxdb.name
+    metrics_influxdb_user     = var.metrics_influxdb.user
+    metrics_influxdb_password = var.metrics_influxdb.password
 
-	application_stacks        = join(",", each.value.config.stacks)
+    application_stacks        = join(",", each.value.config.stacks)
 
-	production                = local.production
+    production                = local.production
   }
 }
 
@@ -37,17 +37,17 @@ resource "aws_instance" "influxdb_servers" {
   availability_zone           = each.value.zone
 
   lifecycle {
-	ignore_changes = [ ami, user_data ]
-	create_before_destroy = true
+    ignore_changes = [ ami, user_data ]
+    create_before_destroy = true
   }
 
   root_block_device {
-	volume_type = "gp2"
-	volume_size = 100
+    volume_type = "gp2"
+    volume_size = 100
   }
 
   tags = {
-	Name = "${each.value.name}"
+    Name = "${each.value.name}"
   }
 }
 
