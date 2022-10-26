d6a28143d65705f82694ebb3f511f0e2995d7136
Jacob Lewallen <jlewallen@gmail.com>
2022-10-26 15:18:56 -0700

    fix: Retry pg started script for about 30s.

--- amis/stacks/postgres-stack/started.sh
+++ amis/stacks/postgres-stack/started.sh
@@ -2,4 +2,16 @@
 
 source /etc/user_data.env
 
-echo "ALTER USER postgres WITH PASSWORD '${FIELDKIT_POSTGRES_DB_PASSWORD}';" | docker exec -i postgres-stack_postgres_1 psql -U postgres
+for i in {1..30}; do
+  sleep 1
+
+  docker exec -i postgres-stack_postgres_1 psql -U postgres -v "ON_ERROR_STOP=1" <<EOF
+ALTER USER postgres WITH PASSWORD '${FIELDKIT_POSTGRES_DB_PASSWORD}';
+EOF
+
+  if [ $? -eq 0 ]; then
+    exit 0
+  else
+    echo "trying again..."
+  fi
+done
