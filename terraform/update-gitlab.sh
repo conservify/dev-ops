ff8c91f2eda5cb9d83e56ed6cfc9d4e30015070b
Jacob Lewallen <jlewallen@gmail.com>
2024-01-05 09:36:01 -0800

    Update with terraform output.

--- terraform/update-gitlab.sh
+++ terraform/update-gitlab.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-SECURE_CONFIG=`pwd`/fk/terraform.tfvars.json
+SECURE_CONFIG=`pwd`/build/deploy.json
 
+echo ${SECURE_CONFIG}
+
+make dev env
 pushd ~/conservify/laborator
-cargo run -- secret --project 48313780 --file $SECURE_CONFIG --key terraform.json
+cargo run -- secret --project 48313780 --file $SECURE_CONFIG --key dev.json
+popd
+
+make prod env
+pushd ~/conservify/laborator
+cargo run -- secret --project 48313780 --file $SECURE_CONFIG --key prod.json
 popd
