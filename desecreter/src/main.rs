d829f68c152ea1a1c014ede5ca32f53680106958
Jacob Lewallen <jlewallen@gmail.com>
2024-01-05 11:29:17 -0800

    Tool for removing passwords from pg_dumps

---
+++ desecreter/src/main.rs
@@ -0,0 +1,69 @@
+use std::str::FromStr;
+
+use anyhow::Result;
+use bcrypt::HashParts;
+use csv::{ReaderBuilder, StringRecord, WriterBuilder};
+
+const BYTES_PREFIX: &str = "\\\\x";
+
+struct GolangBcrypt {}
+
+impl GolangBcrypt {
+    fn from_bytes(bytes: Vec<u8>) -> Result<HashParts> {
+        let as_string = String::from_utf8(bytes.clone())?;
+        Ok(HashParts::from_str(&as_string)?)
+    }
+}
+
+fn sanitize(record: StringRecord, password: &str) -> Result<StringRecord> {
+    if record.len() <= 1 {
+        return Ok(record);
+    }
+
+    Ok(record
+        .into_iter()
+        .map(|field| match field.split_once(BYTES_PREFIX) {
+            Some((_, hex_string)) => match hex::decode(hex_string) {
+                Ok(bytes) => {
+                    GolangBcrypt::from_bytes(bytes).expect("expected bcrypt");
+                    password.to_owned()
+                }
+                Err(_) => field.to_owned(),
+            },
+            None => field.to_owned(),
+        })
+        .collect())
+}
+
+fn make_password() -> Result<String> {
+    let mut field = BYTES_PREFIX.to_owned();
+    field += hex::encode(
+        bcrypt::hash_with_result("asdfasdfasdf", 10)?
+            .to_string()
+            .as_bytes(),
+    )
+    .as_str();
+
+    Ok(field)
+}
+
+fn main() -> Result<()> {
+    let mut reader = ReaderBuilder::new()
+        .delimiter(b'\t')
+        .flexible(true)
+        .from_reader(std::io::stdin());
+
+    let mut writer = WriterBuilder::new()
+        .flexible(true)
+        .from_writer(std::io::stdout());
+
+    let password = make_password()?;
+
+    for row in reader.records() {
+        writer.write_record(&sanitize(row?, &password)?)?;
+    }
+
+    writer.flush()?;
+
+    Ok(())
+}
