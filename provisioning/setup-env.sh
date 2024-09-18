aaec66608cdc41f8e2c14ea74dfd41b1d839cb6c
Jacob Lewallen <jlewallen@gmail.com>
2024-09-18 10:48:27 -0700

    Deleting old jenkins related support scripts/configuration.

--- provisioning/setup-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pushd ../terraform/fk
-source aws.env
-export ENV_DB_URL=`terraform output database_url`
-export FK_CLOUD_ROOT=~/fieldkit/cloud
-popd
--- vars/sayHello.groovy
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String name = 'Jacob') {
-    echo "Hello, ${name}."
-}
