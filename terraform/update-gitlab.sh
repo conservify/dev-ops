290a9479342ed7b082ece331bc6786ffcda18553
Jacob Lewallen <jlewallen@gmail.com>
2025-05-16 15:27:19 -0700

    fix: Path to laborator for updating gitlab secrets.

--- terraform/update-gitlab.sh
+++ terraform/update-gitlab.sh
@@ -5,11 +5,11 @@ SECURE_CONFIG=`pwd`/build/deploy.json
 echo ${SECURE_CONFIG}
 
 make dev env
-pushd ~/conservify/laborator
+pushd ~/fk/tools/laborator
 cargo run -- secret --project 48313780 --file $SECURE_CONFIG --key dev.json
 popd
 
 make prod env
-pushd ~/conservify/laborator
+pushd ~/fk/tools/laborator
 cargo run -- secret --project 48313780 --file $SECURE_CONFIG --key prod.json
 popd
