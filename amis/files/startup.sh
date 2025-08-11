fc3be01ab3f85bc78b517f0b1e11bbf649f90586
Jacob Lewallen <jlewallen@gmail.com>
2025-08-11 13:06:25 -0700

    fix(ami): Mount pg data if pg installed.

--- amis/files/startup.sh
+++ amis/files/startup.sh
@@ -2,11 +2,6 @@
 
 set -xe
 
-source /etc/user_data.env
-
-mkdir -p /tmp/incoming-stacks
-mkdir -p /tmp/downloading-stacks
-
 mkdir -p /svr0 /svr1 /svr2 /svr3 /svr4
 
 if [ -e /dev/xvdh ]; then
@@ -15,22 +10,42 @@ if [ -e /dev/xvdh ]; then
     else
         mkfs.ext4 /dev/xvdh
         mount /dev/xvdh /svr0
+
+	if [ -d /var/lib/postgresql/data ]; then
+		mkdir -p /svr0/postgres
+		mv /var/lib/postgresql/data /svr0/postgres
+		ln -s /svr0/postgres/data /var/lib/postgresql/data
+	fi
     fi
 fi
 
 if [ -e /dev/nvme1n1p1 ]; then
     if mount /dev/nvme1n1p1 /svr0; then
-        echo mounted existing /dev/nvme1n1p1  on /svr0
+        echo mounted existing /dev/nvme1n1p1 on /svr0
     else
         mkfs.ext4 /dev/nvme1n1p1
         mount /dev/nvme1n1p1 /svr0
+
+	if [ -d /var/lib/postgresql/data ]; then
+		mkdir -p /svr0/postgres
+		mv /var/lib/postgresql/data /svr0/postgres
+		ln -s /svr0/postgres/data /var/lib/postgresql/data
+	fi
     fi
 fi
+
+source /etc/user_data.env
+
+mkdir -p /tmp/incoming-stacks
+mkdir -p /tmp/downloading-stacks
 
 pushd /tmp/downloading-stacks
 /var/lib/conservify/startup.py --urls $APPLICATION_STACKS
 mv * /tmp/incoming-stacks
 popd
+
+systemctl enable conservify.service
+systemctl enable conservify.timer
 
 systemctl start conservify.service
 systemctl start conservify.timer
