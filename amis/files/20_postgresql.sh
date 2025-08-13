446e5fe1e3fc4d71b96ecd4f6fc2f045ee80fb00
Jacob Lewallen <jlewallen@gmail.com>
2025-08-13 15:35:05 -0700

    fix(amis): Add timescaledb shared preload libs.

--- amis/files/20_postgresql.sh
+++ amis/files/20_postgresql.sh
@@ -5,6 +5,11 @@ set -xu
 # Enable accepting remote connections.
 sed -i "s@#listen_addresses = 'localhost'@listen_addresses = '*'@g" /etc/postgresql/17/main/postgresql.conf
 
+# Shared library for TimescaleDb
+cat <<EOF >> /etc/postgresql/17/main/postgresql.conf
+shared_preload_libraries = 'timescaledb'
+EOF
+
 # Enable password authentication for remote hosts.
 cat <<EOF >> /etc/postgresql/17/main/pg_hba.conf
 # Password authentication for remote hosts.
