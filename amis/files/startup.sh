9df5fc2cff10daad0b7735da275459f6469a2803
Jacob Lewallen <jlewallen@gmail.com>
2025-08-13 15:35:48 -0700

    feat(amis): External volume added to fstab.

--- amis/files/startup.sh
+++ amis/files/startup.sh
@@ -4,11 +4,6 @@ source /etc/user_data.env
 
 set -xe
 
-if [ ! -f /empty.data ]; then
-	echo creating empty.data for full disk recovery
-	dd if=/dev/zero of=/empty.data count=1024 bs=10M
-fi
-
 DEVICES="/dev/xvdh /dev/nvme1n1"
 
 for device in $DEVICES; do
@@ -22,21 +17,29 @@ for device in $DEVICES; do
 			echo $device: formatting and mounting new disk
 
 			mkfs.ext4 $device
-			mount $device /svr0
 
-			if [ -d /var/lib/postgresql/data ]; then
-				echo $device: rehoming /var/lib/postgresql/data to /svr0/postgres
+			echo $device: adding /etc/fstab row
+			UUID=$(blkid -o value -s UUID $device)
+			cat <<EOF >> /etc/fstab
+UUID=$UUID	/svr0	ext4	defaults	0 0
+EOF
+
+			echo $device: mounting
+			mount /svr0
+
+			if [ -d /var/lib/postgresql/17/main ]; then
+				echo $device: rehoming /var/lib/postgresql/17/main to /svr0/postgres
 				echo $device: stopping postgresql
 				systemctl stop postgresql
-				echo $device: moving /var/lib/postgresql/data
+				echo $device: moving /var/lib/postgresql/17/main
 				mkdir -p /svr0/postgres
-				mv /var/lib/postgresql/data /svr0/postgres
-				ln -s /svr0/postgres/data /var/lib/postgresql/data
+				mv /var/lib/postgresql/17/main /svr0/postgres
+				ln -s /svr0/postgres/main /var/lib/postgresql/17/main
+				chown postgres: /var/lib/postgresql/17/main
 				echo $device: starting postgresql
 				systemctl start postgresql
 			fi
 		fi
-
 
 		break
 	fi
@@ -45,6 +48,12 @@ done
 # If we have a PostgreSQL server set the password.
 if [ -x /usr/lib/postgresql/*/bin/postgres ]; then
 	su - postgres -c "psql -c \"ALTER USER postgres with password '$FIELDKIT_POSTGRES_DB_PASSWORD'\""
+fi
+
+# Large file of nothing we can delete if disk fills up.
+if [ ! -f /empty.data ]; then
+	echo creating empty.data for full disk recovery
+	dd if=/dev/zero of=/empty.data count=1024 bs=10M
 fi
 
 # Download startup stacks and queue them up for loading.
