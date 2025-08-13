7c21d7684a47b1e36b5275fc5141d5e0d0689801
Jacob Lewallen <jlewallen@gmail.com>
2025-08-13 13:27:48 -0700

    refact(amis): Startup/provision and apt sources rework.
    More work done in JSON instead of the provision.sh, with more cohesion
    on setting up a some services, like filebeat. Moved all apt sources to
    one directory and some improvements to startup.sh

---
+++ amis/files/20_postgresql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xu
+
+# Enable accepting remote connections.
+sed -i "s@#listen_addresses = 'localhost'@listen_addresses = '*'@g" /etc/postgresql/17/main/postgresql.conf
+
+# Enable password authentication for remote hosts.
+cat <<EOF >> /etc/postgresql/17/main/pg_hba.conf
+# Password authentication for remote hosts.
+host    all             all             0.0.0.0/0               scram-sha-256
+EOF
+
--- amis/files/startup.sh
+++ amis/files/startup.sh
@@ -1,51 +1,63 @@
 #!/bin/bash
 
-set -xe
-
-mkdir -p /svr0 /svr1 /svr2 /svr3 /svr4
-
-if [ -e /dev/xvdh ]; then
-    if mount /dev/xvdh /svr0; then
-        echo mounted existing /dev/xvdh on /svr0
-    else
-        mkfs.ext4 /dev/xvdh
-        mount /dev/xvdh /svr0
-
-	if [ -d /var/lib/postgresql/data ]; then
-		mkdir -p /svr0/postgres
-		mv /var/lib/postgresql/data /svr0/postgres
-		ln -s /svr0/postgres/data /var/lib/postgresql/data
-	fi
-    fi
-fi
-
-if [ -e /dev/nvme1n1p1 ]; then
-    if mount /dev/nvme1n1p1 /svr0; then
-        echo mounted existing /dev/nvme1n1p1 on /svr0
-    else
-        mkfs.ext4 /dev/nvme1n1p1
-        mount /dev/nvme1n1p1 /svr0
-
-	if [ -d /var/lib/postgresql/data ]; then
-		mkdir -p /svr0/postgres
-		mv /var/lib/postgresql/data /svr0/postgres
-		ln -s /svr0/postgres/data /var/lib/postgresql/data
-	fi
-    fi
-fi
-
 source /etc/user_data.env
 
+set -xe
+
+if [ ! -f /empty.data ]; then
+	echo creating empty.data for full disk recovery
+	dd if=/dev/zero of=/empty.data count=1024 bs=10M
+fi
+
+DEVICES="/dev/xvdh /dev/nvme1n1"
+
+for device in $DEVICES; do
+	echo $device: checking
+	if [ -e $device ]; then
+		mkdir -p /svr0
+
+		if mount $device /svr0; then
+			echo $device: mounted existing on /svr0
+		else
+			echo $device: formatting and mounting new disk
+
+			mkfs.ext4 $device
+			mount $device /svr0
+
+			if [ -d /var/lib/postgresql/data ]; then
+				echo $device: rehoming /var/lib/postgresql/data to /svr0/postgres
+				echo $device: stopping postgresql
+				systemctl stop postgresql
+				echo $device: moving /var/lib/postgresql/data
+				mkdir -p /svr0/postgres
+				mv /var/lib/postgresql/data /svr0/postgres
+				ln -s /svr0/postgres/data /var/lib/postgresql/data
+				echo $device: starting postgresql
+				systemctl start postgresql
+			fi
+		fi
+
+
+		break
+	fi
+done
+
+# If we have a PostgreSQL server set the password.
+if [ -x /usr/lib/postgresql/*/bin/postgres ]; then
+	su - postgres -c "psql -c \"ALTER USER postgres with password '$FIELDKIT_POSTGRES_DB_PASSWORD'\""
+fi
+
+# Download startup stacks and queue them up for loading.
 mkdir -p /tmp/incoming-stacks
 mkdir -p /tmp/downloading-stacks
-
 pushd /tmp/downloading-stacks
 /var/lib/conservify/startup.py --urls $APPLICATION_STACKS
-mv * /tmp/incoming-stacks
+if test -n "$(shopt -s nullglob; echo *)"; then
+	echo moving startup stacks
+	mv * /tmp/incoming-stacks
+else
+	echo no startup stacks found
+fi
 popd
 
-systemctl enable conservify.service
-systemctl enable conservify.timer
-
-systemctl start conservify.service
-systemctl start conservify.timer
+# eof
