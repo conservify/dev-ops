a95ca0cf672bd2d4fdca7a8800caaf10c481eb58
Jacob Lewallen <jlewallen@gmail.com>
2025-09-11 13:58:42 -0700

    feat(amis): Separate sensor data and main backups.

--- amis/files/backup.sh
+++ amis/files/backup.sh
@@ -7,7 +7,9 @@ set -xe
 WORK=/svr0/work
 DATA=/svr0/data
 STAMP=$(date +"%Y%m%d_%H%M%S")
-FILE=${STAMP}_backup.sql
+BASE=${STAMP}_backup.sql
+SENSOR_DATA=${STAMP}_sensor_data.sql
+BOOKMARKS=${STAMP}_bookmarks.sql
 
 pushd $WORK
 
@@ -18,20 +20,18 @@ time pg_dump -v -j1 -d fk \
         --exclude-table-data='fieldkit.ttn_messages' \
         --exclude-table-data='fieldkit.data_record' \
         --exclude-table-data='fieldkit.bookmarks' \
-        > $FILE
+        > $BASE
 
-echo >> $FILE
-echo "SELECT timescaledb_pre_restore();"
-echo >> $FILE
-echo "COPY fieldkit.sensor_data FROM STDIN;" >> $FILE
-time psql -d fk -c "COPY (SELECT * FROM fieldkit.sensor_data WHERE time > NOW() - '7 days'::interval) TO STDOUT" >> $FILE
-echo >> $FILE
-echo "SELECT timescaledb_post_restore();"
-echo >> $FILE
+echo "COPY fieldkit.sensor_data FROM STDIN;" > $SENSOR_DATA
+time psql -d fk -c "COPY (SELECT * FROM fieldkit.sensor_data WHERE time > NOW() - '7 days'::interval) TO STDOUT" >> $SENSOR_DATA
+
+time pg_dump -v -j1 -d fk -t 'fieldkit.bookmarks' > $BOOKMARKS
 
 ls -alh
 
-time xz $FILE
+time xz $BASE
+time xz $SENSOR_DATA
+time xz $BOOKMARKS
 
 mv *.xz $DATA
 
